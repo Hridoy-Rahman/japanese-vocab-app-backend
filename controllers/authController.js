de773f0b0dfb1bebe1eb4b5c12d42285d83b4880
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-12-10 16:59:42 +0600

    user login done

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,3 +17,25 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email,password)
+  
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ message: "User does not exist" });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+  
+      const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+  
+      res.json({ token, user });
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  };
